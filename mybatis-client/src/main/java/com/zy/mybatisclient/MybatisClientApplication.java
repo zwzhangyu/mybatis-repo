09910b8d1fbb75099f0610f6a8ebcbbffd324815
zhangyu <zhangyu_techlife@163>
2024-10-08 22:48:59 +0800

    init

---
+++ mybatis-client/src/main/java/com/zy/mybatisclient/MybatisClientApplication.java
@@ -0,0 +1,13 @@
+package com.zy.mybatisclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisClientApplication.class, args);
+    }
+
+}
---
+++ mybatis-client/src/test/java/com/zy/mybatisclient/MybatisClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.mybatisclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
